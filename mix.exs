497a3240cd13afd34dbfc16630488470ca10cd96
andyl <andy@r210.com>
2026-02-03 16:10:33 -0800

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -11,18 +11,19 @@ defmodule Maint.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:igniter, "~> 0.7"},
+      {:usage_rules, "~> 0.1"},
+      {:jido, "~> 1.1.0-rc.2"},
+      {:jido_ai, "~> 0.5"},
+      {:req_llm, "~> 1.5"},
     ]
   end
 end
